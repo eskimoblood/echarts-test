038fd837d704eaa55c96f473f4a4cfae6fb28bc4
Andreas Köberle <koeberle@gmail.com>
2019-12-27 23:00:53 +0100

    Updates

---
+++ precache-manifest.d76e35cdbefd5b6e78e1a2a7e2af7d55.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91233029c3902332fdb508b0ca3d4d5d",
+    "url": "/echarts-test/index.html"
+  },
+  {
+    "revision": "d02d9a5916f186a77529",
+    "url": "/echarts-test/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "fc905674cca9ee2ae9e1",
+    "url": "/echarts-test/static/js/2.389113e4.chunk.js"
+  },
+  {
+    "revision": "6aeb469c0c20fd3334c86de961c4692c",
+    "url": "/echarts-test/static/js/2.389113e4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d02d9a5916f186a77529",
+    "url": "/echarts-test/static/js/main.c8a473f4.chunk.js"
+  },
+  {
+    "revision": "d9d0bf0e2ab355e9bc55",
+    "url": "/echarts-test/static/js/runtime-main.45cac397.js"
+  }
+]);
